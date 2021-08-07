949507361fc316630e28951168f7627bc617a49c
Patrick the Dipstick <patrick.pattison@gmail.com>
2021-08-06 23:14:17 -0400

    Adding skeleton files for C++, Python, and Rust to 2015 Adding common utils.hpp for 2015.

---
+++ 2015/skel.cpp
@@ -0,0 +1,31 @@
+//
+//  Advent of Code 2016
+//  Day 
+//
+//  By PatchesTheDipstick
+//
+
+
+//  System includes...
+
+
+//  Third Party includes...
+
+
+
+//  Project includes...
+
+#include "../common/utils.hpp"
+
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> input = getInput("input/day.txt");
+	
+	std::cout << "Day  answers:" << std::endl;
+	std::cout << "    Part 1 : " << 0 << std::endl;
+	std::cout << "    Part 2 : " << 0 << std::endl;
+
+	return (0);
+
+}
---
+++ 2015/skel.py
@@ -0,0 +1,24 @@
+#
+#  Advent of Code 2015
+#  Day 
+#
+#  By PatchesTheDipstick
+#
+
+def getInput(file_name):
+	input = []
+	with open(file_name) as input_file:
+		input = input_file.readlines()
+	return input
+
+if __name__ == '__main__':
+	
+	input = getInput("input/day01.txt")
+	
+	
+	part_one = 0
+	part_two = 0
+	print("Advent of Code 2015 Day 03")
+	print("  Part one:", part_one)
+	print("  Part two:", part_two)	
+	
