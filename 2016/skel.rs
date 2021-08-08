f2b0b004be334f1bc3db1e6bd3bc91023fb4f6bb
Patrick the Dipstick <patrick.pattison@gmail.com>
2021-08-07 22:51:00 -0400

    Adding 2016 skeletons.

---
+++ 2016/skel.rs
@@ -0,0 +1,19 @@
+//
+//  Advent of Code 2016
+//  Day 
+//
+//  By PatchesTheDipstick
+//
+use std::env;
+use std::fs;
+
+fn main()
+{
+    let contents = fs::read_to_string("input.txt")
+	    .expect("Something F'ed up!");
+		
+
+	println!("Advent of Code 2016 Day :");
+	println!("  Part 1 : {}", 0);
+	println!("  Part 2 : {}", 0);
+}
