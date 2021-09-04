dbd427a82e9e4e62fb089b32a68b9b564d3e1744
Patrick the Dipstick <patrick.pattison@gmail.com>
2021-09-04 00:55:14 -0400

    Missed the files...

---
+++ 2015/Day 19/day19.cpp
@@ -0,0 +1,31 @@
+//
+//  Advent of Code 2015
+//  Day 
+//
+//  By PatchesTheDipstick
+//
+
+
+//  System includes...
+
+
+//  Third Party includes...
+
+
+
+//  Project includes...
+
+#include "../common/utils.hpp"
+
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> input = ptd::utils::getInput("input.txt");
+	
+	std::cout << "Advent of Code 2015 Day  answers:" << std::endl;
+	std::cout << "    Part 1 : " << 0 << std::endl;
+	std::cout << "    Part 2 : " << 0 << std::endl;
+
+	return (0);
+
+}
---
+++ 2015/Day 25/day25.py
@@ -0,0 +1,44 @@
+#
+#  Advent of Code 2015
+#  Day 
+#
+#  By PatchesTheDipstick
+#
+import re
+
+def getInput(file_name):
+	input = []
+	with open(file_name) as input_file:
+		input = input_file.read().splitlines()
+	return input
+
+if __name__ == '__main__':
+	
+	input = getInput("input.txt")
+	
+	pattern = re.compile('.*row (\d*), column (\d*).')
+	token = pattern.search(input[0])
+	row = int(token.group(1))
+	col = int(token.group(2))
+	
+	last_row = row + col - 1
+	index = 0
+	for a in range(1, last_row):
+		index += a
+	index += col
+	print(index)
+	grid = []
+	value = 20151125
+	for a in range(2, index + 1):
+		grid.append(value)
+		value *= 252533
+		value %= 33554393
+	
+	
+	
+	part_one = value
+	part_two = 0
+	print("Advent of Code 2015 Day 03")
+	print("  Part one:", part_one)
+	print("  Part two:", part_two)	
+	
