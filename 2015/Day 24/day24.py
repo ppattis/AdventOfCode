2c3372ef425c16d4dd6a145c51d1055df1bc7459
Patrick Pattison <patrick.pattison@gmail.com>
2022-01-02 22:55:10 -0500

    Added AoC Day 24 python implamentation.

--- 2015/Day 24/day24.py
+++ 2015/Day 24/day24.py
@@ -1,11 +1,11 @@
 #
 #  Advent of Code 2015
-#  Day 
+#  Day 24
 #
 #  By PatchesTheDipstick
 #
 
-from itertools import permutations
+from itertools import combinations
 
 def getInput(file_name):
 	input = []
@@ -13,36 +13,24 @@ def getInput(file_name):
 		input = input_file.read().splitlines()
 	return input
 
+def find_answer(weights, compartments):
+	targ = sum(weights) // compartments
+	entagles = []
+	for i in range(1, len(weights)):
+		for option in combinations(weights, i):
+			if sum(option) == targ:
+				entagle = 1
+				for x in option:
+					entagle *= x
+				entagles.append(entagle)
+	return min(entagles)
+
 if __name__ == '__main__':
 	
 	input = getInput("input.txt")
 	packages = [int(a) for a in input]
-	total_weight = sum(packages)
-	third_weight = total_weight / 3
-	options = permutations(packages)
-	
-	entaglements = {}
-	for choice in options:
-		left = third_weight
-		new_set = []
-		for a in choice:
-			if a < left:
-				new_set.append(a)
-			elif a == left:
-				new_set.append(a)
-				ent = 1
-				if new_set not in entaglements.keys():
-					for b in new_set:
-						ent *= b
-					entaglements[new_set] = ent
-				break
-			else:
-				break
-	print(entaglements)
-	
-	part_one = 0
-	part_two = 0
-	print("Advent of Code 2015 Day 03")
-	print("  Part one:", part_one)
-	print("  Part two:", part_two)	
+
+	print("Advent of Code 2015 Day 24")
+	print("  Part one:", find_answer(packages, 3))
+	print("  Part two:", find_answer(packages, 4))	
 	
